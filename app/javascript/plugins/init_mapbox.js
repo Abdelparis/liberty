19701c1b45c875ee4398b8b09edf4c5e7f134ad1
Abdelkrim Kiour <akiour@gmail.com>
2020-08-26 22:04:11 +0200

    petites corrections

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,10 +1,5 @@
 import mapboxgl from 'mapbox-gl';
-
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-};
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
@@ -17,6 +12,12 @@ const addMarkersToMap = (map, markers) => {
   });
 };
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -27,7 +28,7 @@ const initMapbox = () => {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
-  markers.forEach((marker) => {
+    markers.forEach((marker) => {
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
@@ -35,6 +36,7 @@ const initMapbox = () => {
    fitMapToMarkers(map, markers);
    addMarkersToMap(map, markers);
   }
+  map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,mapboxgl: mapboxgl }));
 };
 
 
