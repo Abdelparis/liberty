038bc67a158288432c5572f7dd79e353d1809852
christel dufrasne <chbalsacq@gmail.com>
2020-08-27 11:10:39 +0200

    debug javascript homepage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,15 +27,12 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
 import { initMapbox } from '../plugins/init_mapbox';
 import { initNavbar } from '../plugins/navbar';
 
 document.addEventListener('turbolinks:load', () => {
-  initMapbox();
+  if (document.getElementById('map')) {
+    initMapbox();
+  }
   initNavbar();
 })
