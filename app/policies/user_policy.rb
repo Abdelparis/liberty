a50568c0f34977a529a98866532765205cb396be
Abdelkrim Kiour <akiour@gmail.com>
2020-08-31 17:42:36 +0200

    sign up and profile

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,25 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+    def edit?
+    true
+  end
+
+  def update?
+    true
+  end
+
+    private
+
+  def user_is_owner_or_admin?
+    record.user == user
+  end
+end
