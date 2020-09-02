1abc78e51ab38d710c0b828ea22b30f550005461
Abdelkrim Kiour <akiour@gmail.com>
2020-09-02 14:21:52 +0200

    few corrections

--- app/models/job_category.rb
+++ app/models/job_category.rb
@@ -1,5 +1,6 @@
 class JobCategory < ApplicationRecord
-  SUB_CATEGORY = ['Yoga & pilates', 'Fitness']
+  SUB_CATEGORY = ['Yoga', 'Pilates', 'Fitness', 'Coiffure', 'Barbier', 'Manucure', 'Massage', 'Pédicure']
   CATEGORY = ['Esthétique', 'Médecine douce', 'Spa & relaxation', 'Sport']
   validates :category, acceptance: { accept: CATEGORY }
+  validates :name, acceptance: { accept: SUB_CATEGORY }
 end
