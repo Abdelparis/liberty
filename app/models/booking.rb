04c4ccbbe21214db437b2b059e65a36d4de17397
Maelys Bassilekin Blanc <maelysbassilekinblanc@gmail.com>
2020-09-03 22:16:07 +0200

    coquilles

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :mission
   belongs_to :user
+  has_many :chatrooms
   validates :status, acceptance: { accept: ['pending', 'accepted', 'rejected', 'canceled'] }
 end
